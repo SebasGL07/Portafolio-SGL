d7f0866aaae8f2baf9977069c81f918552545b6b
SebasGL07 <sebastiangonzalezlopez2007@outlook.com>
2026-05-21 20:36:58 -0600

    Add files via upload

---
+++ L3-SGL-1252426.cs
@@ -0,0 +1,9 @@
+﻿// Este es un comentario - será ignorado en tiempo de ejecución
+Console.WriteLine("Sebastian Gonzalez Lopez-1252426");
+Console.WriteLine("Laboratorio # 03");
+// Ingresar aquí nombre y carnet del dueño del repositorio
+
+// Ingresar aquí nombre y carnet del colaborador:
+
+Console.WriteLine("Jose Meoño-1116226");
+Console.ReadLine();
